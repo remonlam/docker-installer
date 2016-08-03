906de961872641874dc863eefd4d12b166ef8675
Remon Lam <remon.lam@lamnet.nl>
2016-08-03 15:05:08 +0200

    push initial version for Fedora

---
+++ install-fedora.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## Install Docker on Fedora
+
+# Add the Docker yum repo;
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/fedora/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+# Install the Docker Engine
+dnf install docker-engine
+
+# Enable the Docker service;
+systemctl enable docker.service
+
+# Start the Docker service;
+systemctl start docker
+
+# Show Docker version;
+docker version
+
+# Download en start the Docker "Hello world" container
+docker run hello-world
+
+# Stop and remove ContainerImage;
+docker stop $(docker ps -q)
+docker rm $(docker ps -q)
+
+# Remove image;
+docker rmi $(docker images -q)
